abc7783599363f2f2a8793b2ce07d60e13e186d2
Vladislav0410 <Vladkovalev0410@gmail.com>
2021-02-28 01:21:54 +0300

    update calendar to organizer

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -22,29 +22,30 @@ export class CalendarComponent implements OnInit {
 
   calendar: Week[]
 
-  constructor(private dateService: DateService) { }
+  constructor(public dateService: DateService) { }
 
   ngOnInit() {
     this.dateService.date.subscribe(this.generate.bind(this))
   }
 
-generate(now: moment.Moment) {
-  const startDay = now.clone().startOf('month').startOf('week')
-  const endDay = now.clone().endOf('month').endOf('week')
+  generate(now: moment.Moment) {
+    const startDay = now.clone().startOf('month').startOf('week')
+    const endDay = now.clone().endOf('month').endOf('week')
 
-  const date = startDay.clone().subtract(1, 'day')
+    const date = startDay.clone().subtract(1, 'day')
 
-  const calendar = []
+    const calendar = []
 
-  while (date.isBefore(endDay, 'day')) {
-    calendar.push({
-      days: Array(7)
-        .fill(0)
-        .map(() => {
-          const value = date.add(1, 'day').clone()
-          const active = moment().isSame(value, 'date')
-          const disabled = !now.isSame(value, 'month')
-          const selected = now.isSame(value, 'date')
+
+    while (date.isBefore(endDay, 'day')) {
+      calendar.push({
+        days: Array(7)
+          .fill(0)
+          .map(() => {
+            const value = date.add(1, 'day').clone()
+            const active = moment().isSame(value, 'date')
+            const disabled = !now.isSame(value, 'month')
+            const selected = now.isSame(value, 'date')
 
         return {
           value, active, disabled, selected
@@ -56,5 +57,9 @@ generate(now: moment.Moment) {
   this.calendar = calendar
 
 }
+
+  select(day: moment.Moment) {
+    this.dateService.changeDate(day)
+  }
 
 }
